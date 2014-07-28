83820b44c19b0d7a7ad46653ed2d42bc97086c57
Savva Surenkov <savva.surenkov@outlook.com>
2014-07-29 01:25:40 +1100

    Add Conflicts window, Export command and improve UI

---
+++ Schedule/Windows/ConflictsWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Schedule.Windows
+{
+    public partial class ConflictsWindow : Window
+    {
+        public ConflictsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConflictsWindow_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- Schedule/Windows/MainWindow.xaml.cs
+++ Schedule/Windows/MainWindow.xaml.cs
@@ -1,12 +1,52 @@
-﻿using System.Windows;
+﻿using System;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Schedule.Windows
 {
     public partial class MainWindow : Window
     {
+        private ConflictsWindow _conflicts;
         public MainWindow()
         {
             InitializeComponent();
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
         }
+
+        private void Command_AlwaysExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Window_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_ExportSchedule(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Window_ShowConflicts(object sender, RoutedEventArgs e)
+        {
+            if (_conflicts == null)
+                _conflicts = new ConflictsWindow { Owner = this };
+            _conflicts.Closed += (o, args) => { _conflicts = null; };
+            _conflicts.ShowDialog();
+        }
+    }
+
+    public static class ScheduleCommands
+    {
+        public static readonly RoutedUICommand Export = new RoutedUICommand(
+            "Export in...",
+            "Export",
+             typeof(ScheduleCommands),
+             new InputGestureCollection
+             {
+                 new KeyGesture(Key.E, ModifierKeys.Control)
+             });
     }
 }
